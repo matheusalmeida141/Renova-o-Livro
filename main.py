bc4d07e2ca5c21f7963a9f177a9c8b279070cd02
matheusalmeida141 <117413794+matheusalmeida141@users.noreply.github.com>
2022-11-09 21:34:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,15 +31,18 @@ header = {"Host": "unesp.primo.exlibrisgroup.com",
 #use the 'headers' parameter to set the HTTP headers:
 x = requests.post(url, data = myobj, headers = header)
 
-print(x.text)
+#print(x.json())
 print(x.status_code)
+
+obj = x.json()
+print(obj['jwtData'])
 
 header = {"Host": "unesp.primo.exlibrisgroup.com",
 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
 "Accept": "application/json, text/plain, */*",
 "Accept-Language": "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
 "Accept-Encoding": "gzip, deflate, br",
-"Authorization": "Bearer 'eyJraWQiOiJwcmltYVByaXZhdGVLZXktNTVVTkVTUF9JTlNUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJQcmltYSIsImp0aSI6IjQxOUY1MUZGMzEyMTk1QUVBOUE2Nzg1RTFGMzlGRDlCLmFwZDAxLm5hMDQucHJvZC5hbG1hLmRjMDEuaG9zdGVkLmV4bGlicmlzZ3JvdXAuY29tOjE4MDEiLCJleHAiOjE2Njc5NDg3MjUsImlhdCI6MTY2Nzg2MjMyNSwidXNlck5hbWUiOiI0NTM1NjAzNTg4OCIsImRpc3BsYXlOYW1lIjoiTWF0aGV1cyBGcmFuY2lzY28gZGUgQWxtZWlkYSIsInVzZXIiOiIzNDQ5ODU2NTQwMDA2MzQxIiwidXNlckdyb3VwIjoiMDEiLCJpbnN0aXR1dGlvbiI6IjU1VU5FU1BfSU5TVCIsInVzZXJJcCI6IjIwMC4xNDUuMTY3LjE3OSIsImF1dGhlbnRpY2F0aW9uUHJvZmlsZSI6IkxEQVAiLCJhdXRoZW50aWNhdGlvblN5c3RlbSI6IiIsImxhbmd1YWdlIjoicHQiLCJzYW1sU2Vzc2lvbkluZGV4IjoiIiwic2FtbE5hbWVJZCI6IiIsIm9uQ2FtcHVzIjoiZmFsc2UiLCJzaWduZWRJbiI6InRydWUiLCJ2aWV3SWQiOiI1NVVORVNQX0lOU1Q6VU5FU1AifQ.amb2-G8Iv2zufC3gNXx4vX2Nx6CPKq7MiDvYKH5CQixfPgkKpRr8tzmiD1etP1wDI2TtH9drPtCCLSlBSzSXpg'",
+"Authorization": "Bearer " + obj['jwtData'],
 "DNT": "1",
 "Connection": "keep-alive",
 "Referer": "https://unesp.primo.exlibrisgroup.com/discovery/account?vid=55UNESP_INST:UNESP&section=loans&lang=pt",
